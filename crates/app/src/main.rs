7b7e85b24d23cea7f6442f3ee3da5fd2ed499c3d
misterclayt0n <daviarantes13@gmail.com>
2024-11-20 14:16:43 -0300

    feat: utils crate

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,7 +1,10 @@
 use state::BlinkState;
 use anyhow::Result;
+use utils::init_logging;
 
 fn main() -> Result<()> {
+    init_logging()?;
+
     let mut blink_state = BlinkState::new();
     blink_state.run()
 }
