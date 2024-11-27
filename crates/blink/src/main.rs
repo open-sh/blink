08e19bb5b001ed4c18dc9b5cd294f3409db304fb
misterclayt0n <daviarantes13@gmail.com>
2024-11-27 17:46:05 -0300

    Add basic cli interface setup with clap

--- crates/blink/src/main.rs
+++ crates/blink/src/main.rs
@@ -1,15 +1,29 @@
 // TODO: Add Clap for the cli of this project.
-
 use anyhow::Result;
+use clap::Parser;
+use cli::CLI;
 use config::BlinkConfig;
 use state::BlinkState;
 use utils::init_logging;
 
+mod cli;
+
 fn main() -> Result<()> {
     init_logging()?;
 
-    let config = BlinkConfig::load()?;
+    let cli = CLI::parse();
 
-    let mut blink_state = BlinkState::new(config)?;
-    blink_state.run()
+    match &cli.commands {
+        Some(cli::Commands::Test) => {
+            println!("gotcha bitch");
+        }
+        None => {
+            let config = BlinkConfig::load()?;
+
+            let mut blink_state = BlinkState::new(config)?;
+            blink_state.run()?;
+        }
+    }
+
+    Ok(())
 }
